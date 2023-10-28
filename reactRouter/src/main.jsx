8f18a8d42762342ee00634f4d66154f1b52536f4
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2023-10-28 23:39:05 +0600

    React Router Project

---
+++ reactRouter/src/main.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import Layout from "./Layout.jsx";
+import Home from "./components/Home/Home.jsx";
+import About from "./components/About/About.jsx";
+import Contact from "./components/Contact/Contact.jsx";
+import User from "./components/User/User.jsx";
+
+/*const router = createBrowserRouter([
+    {
+        path : '/',
+        element: <Layout/>,
+        children:[
+            {
+                path:"",
+                element:<Home/>
+            },
+            {
+                path:"about",
+                element:<About/> //Which component or element to render when the path matches.
+            },
+            {
+                path:"contact",
+                element:<Contact/>
+            },
+            {
+                path : 'user/:userid',
+                element : <User/>
+            }
+
+        ],
+    }
+])*/
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<Layout />}>
+            <Route path='' element={<Home />} />
+            <Route path='about' element={<About />} />
+            <Route path='contact' element={<Contact />} />
+            <Route path='user/:userid' element={<User />} />
+        </Route>
+    )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>,
+)
