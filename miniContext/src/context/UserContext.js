0e9e8b312748f293ae32657d2d9a3b0f098bab5b
Chayan Chandro Sutrodhar <chayansd5656@gmail.com>
2023-11-02 17:13:19 +0600

    react context api implementation

---
+++ miniContext/src/context/UserContext.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const UserContext = React.createContext();
+
+
+export default UserContext;
+
+
+/*
+    1. Create a context object
+    2. Export the context object
+    3. Import the context object into the component that will provide the context
+    4.This is my global variable that I want to share with other components.
+ */
